58c333679dbfffc4040649981898a5e9dc54a91a
Excalibur <abenezer.tariku2015@gmail.com>
2018-12-15 14:50:45 +0300

    first commit

---
+++ GithubProject/src/main/java/com/abeneyigitsay/www/GithubProject/App.java
@@ -0,0 +1,13 @@
+package com.abeneyigitsay.www.GithubProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
